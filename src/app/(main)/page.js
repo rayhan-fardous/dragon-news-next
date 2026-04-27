a03bbb6914bd9e0655d1644a83b624c456120997
Md Rayhan ul Fardous <49313477+rayhan-fardous@users.noreply.github.com>
2026-04-27 22:39:40 +0600

    Design Grid Layout, Fetch Categories

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,5 +1,28 @@
+async function getCategories() {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/news/categories",
+  );
+  const data = await res.json();
+  return data.data;
+}
 
+export default async function Home() {
+  const categories = await getCategories();
 
-export default function Home() {
-  return <h2 className="text-red-500">Homepage</h2>;
+  return (
+    <div className="container mx-auto grid grid-cols-12 gap-4 my-15">
+      <div className="col-span-3">
+        <h2 className="font-bold text-lg">All categories</h2>
+        <ul className="flex flex-col gap-3 mt-6">
+          {categories.news_category.map((category) => {
+            return <li key={category.category_id} className="bg-slate-100 p-2 rounded-md font-bold text-center text-gray-600 text-md">{category.category_name}</li>;
+          })}
+        </ul>
+      </div>
+      <div className="font-bold text-3xl bg-purple-100 col-span-6">
+        All News
+      </div>
+      <div className="font-bold text-3xl bg-yellow-100 col-span-3">Social</div>
+    </div>
+  );
 }
